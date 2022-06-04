13bd5420321b58c7a9b35e151f32d2b83aa28d09
Laur1502 <48560664+Laur1502@users.noreply.github.com>
2022-06-05 00:14:16 +0200

    design page démarrage quizz

--- quiz-ui/src/views/NewQuizPage.vue
+++ quiz-ui/src/views/NewQuizPage.vue
@@ -1,10 +1,15 @@
 <template>
-  <h1>Start new quiz page</h1>
-  <p>Saisissez votre nom :</p>
-  <input type="text" placeholder="Username" v-model="username" />
-  <button type="button" class="btn btn-outline-danger" @click="launchNewQuiz">
-    GO!
-  </button>
+
+  <div class="main-container flex-column">
+    <h2>Nouvelle partie</h2>
+    <p>Saisissez votre nom :</p>
+    <div class="input-container">
+      <input type="text" placeholder="Username" v-model="username" />
+      <button type="button" class="btn btn-outline-primary btn-grey" @click="launchNewQuiz">
+        GO!
+      </button>
+    </div>
+  </div>
 </template>
 
 <script>
@@ -28,4 +33,47 @@ export default {
 };
 </script>
 
-<style></style>
+<style>
+@font-face {
+  font-family: "Disney";
+  src: url("../assets/waltograph42.otf");
+}
+
+.main-container h2 {
+  margin-bottom: 4%;
+}
+
+.input-container {
+  display: flex;
+  margin: 0 auto;
+  width: 50%;
+  justify-content: space-around;
+}
+
+.input-container input {
+  height: 100%;
+  padding: 0;
+  width: 60%;
+  height: 35px;
+}
+
+.input-container input[type=text] {
+  padding: 12px 10px;
+  box-sizing: border-box;
+  color: lightslategray;
+}
+
+.input-container input[type=text]:focus {
+  outline: none;
+}
+
+.input-container input:focus::placeholder {
+  color: transparent;
+}
+
+.input-container button {
+  border-radius: 0%;
+  margin: 0;
+  width: 30%;
+}
+</style>
