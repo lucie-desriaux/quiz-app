d0564a0ccc98273a90f6da859bd319057745b78a
Laur1502 <48560664+Laur1502@users.noreply.github.com>
2022-06-06 14:01:09 +0200

    Page liste question terminée

--- quiz-ui/src/views/QuestionsList.vue
+++ quiz-ui/src/views/QuestionsList.vue
@@ -1,42 +1,23 @@
 <template>
   <div class="m-5">
-    <h2 class="text-center">
-      Administration - Consultation de toutes les questions
-    </h2>
-    <div class="">
+    <h3 class="text-center m-0">
+      Consultation de toutes les questions
+    </h3>
+    <div class="container-questions-libelle">
       <!-- <h3 class="text-center">Toutes les questions</h3> -->
-      <div class="all-questions" v-for="q in questions" v-bind:key="q.position">
-        <h4>
+      <div class="all-questions d-flex" v-for="q in questions" v-bind:key="q.position">
+        <a class="questions-libelle" @click="$emit('show-question', q.position)">
           <span class="numero">{{ q.position }}.</span> {{ q.title }} -
           {{ q.text }}
-        </h4>
-        <div class="container-img-rep">
-          <div class="cont-img">
-            <img width="" v-if="q.image" :src="q.image" />
-          </div>
-          <div class="cont-reponses d-flex flex-column">
-            <div
-              class="all-possibleAnswers"
-              v-for="pa in q.possibleAnswers"
-              v-bind:key="pa.index"
-            >
-              <input
-                type="checkbox"
-                id="isCorrect"
-                v-model="pa.isCorrect"
-                disabled
-              />
-              <label for="isCorrect"> {{ pa.text }} </label>
-            </div>
-          </div>
-        </div>
-        <button
-          type="button"
-          class="btn btn-outline-primary btn-grey"
-          @click="$emit('show-question', q.position)"
-        >
+          <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-pencil-fill"
+            viewBox="0 0 16 16">
+            <path
+              d="M12.854.146a.5.5 0 0 0-.707 0L10.5 1.793 14.207 5.5l1.647-1.646a.5.5 0 0 0 0-.708l-3-3zm.646 6.061L9.793 2.5 3.293 9H3.5a.5.5 0 0 1 .5.5v.5h.5a.5.5 0 0 1 .5.5v.5h.5a.5.5 0 0 1 .5.5v.5h.5a.5.5 0 0 1 .5.5v.207l6.5-6.5zm-7.468 7.468A.5.5 0 0 1 6 13.5V13h-.5a.5.5 0 0 1-.5-.5V12h-.5a.5.5 0 0 1-.5-.5V11h-.5a.5.5 0 0 1-.5-.5V10h-.5a.499.499 0 0 1-.175-.032l-.179.178a.5.5 0 0 0-.11.168l-2 5a.5.5 0 0 0 .65.65l5-2a.5.5 0 0 0 .168-.11l.178-.178z" />
+          </svg>
+        </a>
+        <!-- <button type="button" class="" @click="$emit('show-question', q.position)">
           Détails
-        </button>
+        </button> -->
       </div>
     </div>
   </div>
@@ -73,6 +54,10 @@ h4 {
   font-weight: bold;
 }
 
+.container-questions-libelle {
+  margin: 2% 0 2% 0;
+}
+
 .numero {
   font-size: 1.3em;
 }
@@ -101,5 +86,21 @@ h4 {
 
 .all-questions button {
   margin-top: 2%;
+}
+
+.questions-libelle {
+  cursor: pointer;
+  text-decoration: none;
+  color: black;
+  /* margin-top: 1; */
+}
+
+.questions-libelle:hover {
+  background-color: transparent;
+  color: #d393eb;
+}
+
+.bi-pencil-fill {
+  color: lightcoral;
 }
 </style>
